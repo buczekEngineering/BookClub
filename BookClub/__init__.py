09baf6a0c22297f777db9a97211e6b2ac880844c
buczekEngineering <79086394+buczekengineering@users.noreply.github.com>
2022-02-09 11:37:23 +0100

    added package structure

---
+++ BookClub/__init__.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+# one of the configurations key that flask is using
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:3004@localhost:5432/books'
+# connect app to db
+db = SQLAlchemy(app)
